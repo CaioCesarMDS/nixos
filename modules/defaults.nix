97a8ad894c1311598a66a4d844fc8acf05f089cd
caiocsx <caiocesarsts@gmail.com>
2026-07-25 23:38:37 -0300

    fix: remove backup file extension setting

--- modules/defaults.nix
+++ modules/defaults.nix
@@ -2,9 +2,6 @@
 {
   den.default.nixos.system.stateVersion = "25.11";
   den.default.homeManager.home.stateVersion = "25.11";
-
-  den.default.nixos.home-manager.backupFileExtension = "hm-backup";
-  
   den.schema.user.classes = lib.mkDefault [ "homeManager" ];
 
   den.default.includes = [
