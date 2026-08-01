9f7d919c60dda4a50619e44934894576321b6abe
caiocsx <caiocesarsts@gmail.com>
2026-08-01 16:37:56 -0300

    refactor(modules): reorganize modules under aspects

--- modules/aspects/desktop/wallpapers.nix
+++ modules/aspects/desktop/wallpapers.nix
@@ -3,7 +3,7 @@
   den.aspects.wallpapers.homeManager = { pkgs, ... }: {
     home.packages = with pkgs; [ awww ];
     home.file."Pictures/Wallpapers" = {
-      source = ../../assets/wallpapers;
+      source = ../../../assets/wallpapers;
       recursive = true;
     };
 
