cf2e870b5ecc504f05cfc73ce340616a8ed880ee
caiocsx <caiocesarsts@gmail.com>
2026-08-04 09:10:01 -0300

    feat(nix): add Stylix flake input

--- flake.nix
+++ flake.nix
@@ -24,6 +24,10 @@
       url = "github:Gerg-L/spicetify-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs = {
