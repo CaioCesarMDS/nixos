54b15e85bcc8188a198f42d8b02ff6e2820eb844
caiocsx <caiocesarsts@gmail.com>
2026-08-14 09:34:15 -0300

    feat(ly): add configurable animation

--- modules/aspects/services/ly.nix
+++ modules/aspects/services/ly.nix
@@ -1,23 +1,47 @@
 { ... }:
 {
   den.aspects.ly.nixos =
-    { pkgs, ... }:
     {
-      services.displayManager = {
-        defaultSession = "hyprland";
-        ly = {
-          enable = true;
-          x11Support = false;
-          settings = {
-            animation = "matrix"; # colormix, doom, gameoflife
-            clock = "%H:%M";
-            hide_borders = true;
-            hide_key_hints = false;
-            save = true;
-            load = true;
-            bigclock = true;
-            default_input = "password";
-            clear_password = true;
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      cfg = config.services.displayManager.ly;
+    in
+    {
+      options.services.displayManager.ly.animation = lib.mkOption {
+        type = lib.types.nullOr (
+          lib.types.enum [
+            "none"
+            "colormix"
+            "doom"
+            "gameoflife"
+            "matrix"
+          ]
+        );
+        default = "matrix";
+        description = "Background animation for the Ly display manager.";
+      };
+
+      config = {
+        services.displayManager = {
+          defaultSession = "hyprland";
+          ly = {
+            enable = true;
+            x11Support = false;
+            settings = {
+              animation = cfg.animation;
+              clock = "%H:%M";
+              hide_borders = true;
+              hide_key_hints = false;
+              save = true;
+              load = true;
+              bigclock = true;
+              default_input = "password";
+              clear_password = true;
+            };
           };
         };
       };
