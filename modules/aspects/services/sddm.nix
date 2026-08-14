fbc1ede7b67fc37c5e29470064f451cc2fd2358a
caiocsx <caiocesarsts@gmail.com>
2026-08-14 09:34:24 -0300

    feat(sddm): add configurable theme

--- modules/aspects/services/sddm.nix
+++ modules/aspects/services/sddm.nix
@@ -1,30 +1,59 @@
 { ... }:
 {
   den.aspects.sddm.nixos =
-    { pkgs, ... }:
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
 
     let
+      cfg = config.services.displayManager.sddm.astronaut;
+
       customSddmAstronaut = pkgs.sddm-astronaut.override {
-        embeddedTheme = "pixel_sakura";
+        embeddedTheme = cfg.theme;
       };
+
     in
     {
-      environment.systemPackages = [
-        customSddmAstronaut
-      ];
-
-      services.displayManager = {
-        defaultSession = "hyprland";
-        sddm = {
-          enable = true;
-          wayland.enable = true;
-          package = pkgs.kdePackages.sddm;
-          theme = "sddm-astronaut-theme";
-          extraPackages = with pkgs; [
-            kdePackages.qtsvg
-            kdePackages.qtvirtualkeyboard
-            kdePackages.qtmultimedia
+      options.services.displayManager.sddm.astronaut = {
+        theme = lib.mkOption {
+          type = lib.types.enum [
+            "astronaut"
+            "black_hole"
+            "cyberpunk"
+            "hyprland_kath"
+            "jake_the_dog"
+            "japanese_aesthetic"
+            "pixel_sakura"
+            "pixel_sakura_static"
+            "post-apocalyptic_hacker"
+            "purple_leaves"
           ];
+          default = "pixel_sakura";
+          description = "Built-in theme for sddm-astronaut.";
+        };
+      };
+
+      config = {
+        environment.systemPackages = [
+          customSddmAstronaut
+        ];
+
+        services.displayManager = {
+          defaultSession = "hyprland";
+          sddm = {
+            enable = true;
+            wayland.enable = true;
+            package = pkgs.kdePackages.sddm;
+            theme = "sddm-astronaut-theme";
+            extraPackages = with pkgs; [
+              kdePackages.qtsvg
+              kdePackages.qtvirtualkeyboard
+              kdePackages.qtmultimedia
+            ];
+          };
         };
       };
     };
