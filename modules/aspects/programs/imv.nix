20d6b8534b18d8d63ff148ac0d6846dbcb64e16d
caiocsx <caiocesarsts@gmail.com>
2026-09-03 21:09:43 -0300

    feat(imv): open images from current directory

--- modules/aspects/programs/imv.nix
+++ modules/aspects/programs/imv.nix
@@ -2,18 +2,67 @@
 {
   den.aspects.imv.homeManager =
     { pkgs, ... }:
+    let
+      imv-dir = pkgs.writeShellScriptBin "imv-dir" ''
+        file=$(realpath "$1")
+        dir=$(dirname "$file")
+
+        shopt -s nullglob nocaseglob
+
+        files=()
+        for f in "$dir"/*; do
+          if [ -f "$f" ]; then
+            files+=("$f")
+          fi
+        done
+
+        if [ ''${#files[@]} -eq 0 ]; then
+          exec ${pkgs.imv}/bin/imv "$file"
+        fi
+
+        index=1
+        for i in "''${!files[@]}"; do
+          if [ "$(realpath "''${files[$i]}")" = "$file" ]; then
+            index=$((i + 1))
+            break
+          fi
+        done
+
+        exec ${pkgs.imv}/bin/imv -n "$index" "''${files[@]}"
+      '';
+    in
     {
       programs.imv = {
         enable = true;
       };
 
-      xdg.mimeApps.defaultApplications = {
-        "image/jpeg" = "imv.desktop";
-        "image/png" = "imv.desktop";
-        "image/gif" = "imv.desktop";
-        "image/svg+xml" = "imv.desktop";
-        "image/webp" = "imv.desktop";
-        "image/heif" = "imv.desktop";
+      xdg = {
+        desktopEntries.imv-folder = {
+          name = "imv (Folder Viewer)";
+          exec = "${imv-dir}/bin/imv-dir %f";
+          mimeType = [
+            "image/jpeg"
+            "image/png"
+            "image/gif"
+            "image/webp"
+            "image/bmp"
+            "image/tiff"
+            "image/svg+xml"
+            "image/heif"
+          ];
+          noDisplay = true;
+        };
+
+        mimeApps.defaultApplications = {
+          "image/jpeg" = "imv-folder.desktop";
+          "image/png" = "imv-folder.desktop";
+          "image/gif" = "imv-folder.desktop";
+          "image/webp" = "imv-folder.desktop";
+          "image/bmp" = "imv-folder.desktop";
+          "image/tiff" = "imv-folder.desktop";
+          "image/svg+xml" = "imv-folder.desktop";
+          "image/heif" = "imv-folder.desktop";
+        };
       };
     };
 }
